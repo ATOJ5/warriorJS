42718583563b2c2c4e3bfa66edbd2dd00cc5ee70
Jose David García Corzo <108841509+ATOJ5@users.noreply.github.com>
2023-09-24 21:50:07 +0200

    Create level5.js

---
+++ level5.js
@@ -0,0 +1,35 @@
+class Player {
+  constructor() {
+    this.health = 20;
+    this.currentHealth = 20;
+    this.attack = 5;
+  }
+
+  fullHealth(warrior) {
+    return warrior.health() === this.health;
+  }
+
+  takingDamage(warrior) {
+    return warrior.health() < this.currentHealth;
+  }
+
+  walkOrRest(warrior) {
+    return this.takingDamage(warrior) || this.fullHealth(warrior)
+      ? warrior.walk()
+      : warrior.rest();
+  }
+
+  attackOrRescue(warrior){
+    return warrior.feel().getUnit().isEnemy() 
+    ? warrior.attack()
+    : warrior.rescue();
+  }
+
+  playTurn(warrior) {
+    warrior.feel().isEmpty()
+      ? this.walkOrRest(warrior)
+      : this.attackOrRescue(warrior)
+
+    this.currentHealth = warrior.health();
+  }
+}
